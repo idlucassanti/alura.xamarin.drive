88c076185cf2cead87ba114942dc72a89bd57af8
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2022-11-09 14:34:07 -0300

    #Components: Listagem com componente ListView

--- src/Alura.TestDrive/Alura.TestDrive/MainPage.xaml.cs
+++ src/Alura.TestDrive/Alura.TestDrive/MainPage.xaml.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Alura.TestDrive.Models;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Alura.TestDrive
 {
     public partial class MainPage : ContentPage
     {
+        public List<VeiculoModel> Veiculos { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            Veiculos = new List<VeiculoModel>();
+
+            Veiculos.Add(new VeiculoModel(1, "Azera", 60000));
+            Veiculos.Add(new VeiculoModel(2, "HB20", 55000));
+            Veiculos.Add(new VeiculoModel(3, "Fiesta Turno", 45000));
+
+            BindingContext = this;
         }
     }
 }
---
+++ src/Alura.TestDrive/Alura.TestDrive/Models/VeiculoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Alura.TestDrive.Models
+{
+    public class VeiculoModel
+    {
+        public VeiculoModel(int id, string nome, decimal valor)
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
